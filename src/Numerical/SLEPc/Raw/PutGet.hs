30d765ed43d946528dde5d46e5d21588523e6df0
ocramz <zocca.marco@gmail.com>
2015-10-01 00:07:09 +0200

    added stuff in Raw/PutGet

--- src/Numerical/SLEPc/Raw/PutGet.hs
+++ src/Numerical/SLEPc/Raw/PutGet.hs
@@ -33,21 +33,25 @@ withEpsSetup comm matA matB ty post = withEps comm $ \e -> do
   chk0 $ epsSetup' e
   post e
 
+withEpsSetupSolve :: Comm -> Mat -> Mat -> EpsProblemType_ -> (EPS -> IO a) -> IO a
 withEpsSetupSolve comm a b ty postsolve = withEpsSetup comm a b ty $ \e -> do
   chk0 $ epsSolve' e
   postsolve e
 
+epsIsHermitian, epsIsPositive :: EPS -> IO PetscBool_
 epsIsHermitian = chk1 . epsIsHermitian'
-
 epsIsPositive = chk1 . epsIsPositive'
 
+epsSetDimensions :: EPS -> Int -> Int -> Int -> IO ()
 epsSetDimensions e nev ncv mpd  = chk0 $ epsSetDimensions' e nev ncv mpd
 
+epsSetInterval :: EPS -> PetscReal_ -> PetscReal_ -> IO ()
 epsSetInterval e smin smax = chk0 $ epsSetInterval' e smin smax
 
 
 
 
+epsView :: EPS -> PetscViewer -> IO ()
 epsView e viewer = chk0 $ epsView' e viewer
 
 
@@ -60,12 +64,16 @@ epsView e viewer = chk0 $ epsView' e viewer
 
 -- * SLEPc misc
 
+slepcInit0, slepcFin :: IO ()
 slepcInit0 = chk0 slepcInit01
 slepcFin = chk0 slepcFin1
 
+
+-- | FIXME: move into specialized monad
+withSlepc0 :: IO a -> IO a
 withSlepc0 = bracket_ slepcInit0 slepcFin
 
 
-
+commWorld, commSelf :: Comm
 commWorld = commWorld1
 commSelf = commSelf1
