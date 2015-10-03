c5bbc538cfec6abc65ff2fa167d76a77147f16fc
ocramz <zocca.marco@gmail.com>
2015-10-03 12:19:50 +0200

    FIXME: EPS state, see t3

--- src/Numerical/SLEPc/TestMain.hs
+++ src/Numerical/SLEPc/TestMain.hs
@@ -31,3 +31,17 @@ t2' = withMatCreateSeqAIJConstNZPR cw 3 3 3 $ \pm -> do
       cw = commWorld
 
 t2 = withSlepc0 t2'
+
+
+t3' = withMatCreateSeqAIJConstNZPR cw 3 3 3 $ \pm -> do
+  let m = petscMatrixMat pm -- last time copying all this stuff, will abstract out
+  matNewNZallocErrorOff m -- <- need to figure this one out
+  matSetup m -- <- this should be a given
+  matSetValueArraySafe pm [0,0,0,1,1,1,2,2,2] [0,1,2,0,1,2,0,1,2] (replicate 9 pi) InsertValues
+  matAssembly m
+  withEpsSetupSolve cw m m EpsHep $ \eps ->
+    epsIsHermitian eps
+     where
+       cw = commWorld
+
+t3 = withSlepc0 t3'
